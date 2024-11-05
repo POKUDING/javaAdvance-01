206e5296922f9b8e78234c03a87ec4892a2ab6b0
POKUDING <yoseb95@gmail.com>
2024-11-05 11:03:55 +0900

    관심상품 등록 API

--- src/test/java/com/sparta/myselectshop/MyselectshopApplicationTests.java
+++ src/test/java/com/sparta/myselectshop/MyselectshopApplicationTests.java
@@ -2,6 +2,7 @@ package com.sparta.myselectshop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootTest
 class MyselectshopApplicationTests {
